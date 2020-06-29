13ef7c78fef8474b039facdf9228c096d6bdae95
mateoox600 <matboi862@gmail.com>
2020-06-29 19:07:22 +0200

    t

--- src/main/java/fr/mateoox600/mcb/events/EnderBotMiningEvents.java
+++ src/main/java/fr/mateoox600/mcb/events/EnderBotMiningEvents.java
@@ -11,17 +11,15 @@ public class EnderBotMiningEvents extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent e) {
-        if (e.getChannel().getId().equals("722010652213772379") && !e.getMessage().getEmbeds().isEmpty()) {
+        if (e.getAuthor().getId().equals("280726849842053120") && e.getChannel().getId().equals("722010652213772379") && !e.getMessage().getEmbeds().isEmpty()) {
             MessageEmbed msg_embed = e.getMessage().getEmbeds().get(0);
             try {
-                if (msg_embed.getAuthor().getName().equalsIgnoreCase(e.getJDA().getUserById(MCB.config.getOwnerId()).getName())) {
-                    StringBuilder trade = new StringBuilder(">trade");
-                    MessageEmbed.Field field = msg_embed.getFields().get(0);
-                    for (String ress : field.getValue().split("\n"))
-                        trade.append(" ").append(ress.split(":")[1]).append(" ").append(Long.valueOf(String.join("", ress.split(":")[3].split(" "))));
-                    trade.append(" @");
-                    e.getChannel().sendMessage("```" + trade.toString() + "```").queue();
-                }
+                StringBuilder trade = new StringBuilder(">trade");
+                MessageEmbed.Field field = msg_embed.getFields().get(0);
+                for (String ress : field.getValue().split("\n"))
+                    trade.append(" ").append(ress.split(":")[1]).append(" ").append(Long.valueOf(String.join("", ress.split(":")[3].split(" "))));
+                trade.append(" @");
+                e.getChannel().sendMessage("```" + trade.toString() + "```").queue();
             }catch (Exception ignored){
             }
         }
